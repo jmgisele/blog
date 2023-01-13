eb1a16ffc8d7b17dac83b9cb0a60954648733f0b
James Gisele <james.may.gisele@protonmail.com>
2023-01-12 19:16:16 -0500

    restructuring

---
+++ src/metadata/blog_entries.js
@@ -0,0 +1,10 @@
+export const metadata = {
+	blog_entries: [
+		{
+			slug: 'hello_world',
+			title: 'hello_world',
+			description: 'beginnings',
+			published: 'January 09, 2023 00:00:00'
+		}
+	]
+};
