18c8e5edde64756bb5c55fee9e872dcd4f091e95
James Gisele <james.may.gisele@protonmail.com>
2023-04-01 12:34:12 -0400

    fixing build errors

---
+++ src/routes/trivia/+page.svelte
@@ -0,0 +1,13 @@
+<script>
+	import BasicWrapper from '../../lib/wrappers/BasicWrapper.svelte';
+	import BodyWrapper from '../../lib/wrappers/BodyWrapper.svelte';
+</script>
+
+<BasicWrapper navbar="true">
+	<BodyWrapper>
+		<h1>coming soon</h1>
+	</BodyWrapper>
+</BasicWrapper>
+
+<style>
+</style>
