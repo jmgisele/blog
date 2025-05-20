0e1ced300120bd5fcdcf0e71ec8add8881dc6989
James Gisele <james.may.gisele@protonmail.com>
2025-05-20 15:03:43 -0400

    div footer

--- src/lib/text_styling/ClipboardTooltip.svelte
+++ src/lib/text_styling/ClipboardTooltip.svelte
@@ -79,9 +79,9 @@
 >
 	<div class="tooltip" {id}>
 		{tooltipText} <br />
-		<!-- <span>
+		<span>
 			{#if isClicked}
-				<svg
+				<!-- <svg
 					xmlns="http://www.w3.org/2000/svg"
 					width="16"
 					height="16"
@@ -99,10 +99,10 @@
 					<path
 						d="M9.5 1a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-3a.5.5 0 0 1-.5-.5v-1a.5.5 0 0 1 .5-.5h3zm-3-1A1.5 1.5 0 0 0 5 1.5v1A1.5 1.5 0 0 0 6.5 4h3A1.5 1.5 0 0 0 11 2.5v-1A1.5 1.5 0 0 0 9.5 0h-3z"
 					/>
-				</svg>
+				</svg> -->
 			{:else}
 				double click!
-				<svg
+				<!-- <svg
 					xmlns="http://www.w3.org/2000/svg"
 					width="16"
 					height="16"
@@ -116,10 +116,9 @@
 					<path
 						d="M9.5 1a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-3a.5.5 0 0 1-.5-.5v-1a.5.5 0 0 1 .5-.5h3zm-3-1A1.5 1.5 0 0 0 5 1.5v1A1.5 1.5 0 0 0 6.5 4h3A1.5 1.5 0 0 0 11 2.5v-1A1.5 1.5 0 0 0 9.5 0h-3z"
 					/>
-				</svg>
+				</svg> -->
 			{/if}
 		</span>
-	</div> -->
 	</div>
 	<slot />
 </button>
