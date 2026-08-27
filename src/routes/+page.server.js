08a86293598f9e93dd8e828e12a3e9d4093e884b
James Gisele <james.may.gisele@protonmail.com>
2026-08-27 12:56:23 -0400

    cleanup

--- src/routes/+page.server.js
+++ src/routes/+page.server.js
@@ -2,7 +2,9 @@ import { metadata } from '../metadata/blog_entries';
 
 export async function load() {
 	return {
-		entries: metadata.blog_entries.sort((a, b) => {
+		entries: metadata.blog_entries
+			.filter(e => !e.title.includes("Luddite")) // don't worry you can still see these posts if you go to the direct url :') 
+			.sort((a, b) => {
 			return new Date(a.published) < new Date(b.published) ? 1 : -1;
 		})
 	};
